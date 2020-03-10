9f143c64500ba4f858af65cea7eed0c9cf0d83cc
David N. Miller <48084926+Millertoto@users.noreply.github.com>
2020-03-10 10:25:41 +0100

    Add project files.

---
+++ BogLib/Bog.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace BogLib
+{
+    public class Bog
+    {
+        private string _titel;
+        private string _forfatter;
+        private int _sidetal;
+        private string _isbn13;
+
+        public Bog() { }
+
+        public Bog(string titel, string forfatter, int sidetal, string isbn13)
+        {
+            _titel = titel;
+            _forfatter = forfatter;
+            _sidetal = sidetal;
+            _isbn13 = isbn13;
+        }
+
+        public string Titel
+        {
+            get { return _titel; }
+            set { _titel = value; }
+        }
+        public string Forfatter
+        {
+            get { return _forfatter; }
+            set
+            {
+                string spacesremoved = value.Replace(" ", "");
+                if (spacesremoved.Length >= 2)
+                {
+                    _forfatter = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+
+        }
+        public int Sidetal {
+            get { return _sidetal; }
+            set
+            {
+                if (value >= 4 && value <= 1000)
+                {
+                    _sidetal = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+             }
+
+        public string Isbn13
+        {
+            get { return _isbn13; }
+            set
+            {
+                if (value.Length == 13)
+                {
+                    _isbn13 = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Titel:{Titel} Forfatter:{Forfatter} Sidetal:{Sidetal} Isbn13:{Isbn13}";
+        }
+    }
+}
---
+++ BogRest/Controllers/BogController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BogLib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BogRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BogController : ControllerBase
+    {
+        private static readonly List<Bog> bogs = new List<Bog>()
+        {
+            new Bog("Starry night", "MoonMoon", 420, "123456789moon"),
+            new Bog("Long kiss goodbye", "I don't know", 500, "1133224455678"),
+            new Bog("Artemis Fowl", "Eoin Colfer", 390, "2135468791234")
+            
+        };
+            // GET: api/Bog
+        [HttpGet]
+        public IEnumerable<Bog> Get()
+        {
+            return bogs;
+        }
+
+        // GET: api/Bog/5
+        [HttpGet("{id}", Name = "Get")]
+        public Bog Get(string id)
+        {
+            return bogs.Find(i =>i.Isbn13 == id);
+        }
+
+        // POST: api/Bog
+        [HttpPost]
+        public void Post([FromBody] Bog value)
+        {
+        }
+
+        // PUT: api/Bog/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Bog value)
+        {
+            Bog bog = Get(id);
+            if (bog != null)
+            {
+                bog.Isbn13 = value.Isbn13;
+                bog.Forfatter = value.Forfatter;
+                bog.Sidetal = value.Sidetal;
+                bog.Titel = value.Titel;
+            }
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            Bog bog = Get(id);
+            bogs.Remove(bog);
+        }
+    }
+}
